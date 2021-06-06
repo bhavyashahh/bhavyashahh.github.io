8e5a768a42f18b53f3d844016c0dbc2da3ca8dbc
bhavyashahh <42242223+bhavyashahh@users.noreply.github.com>
2021-06-07 01:44:51 +0530

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -6,7 +6,7 @@ var LANGID=null;
 var TWEET_ID=null;
 if (window.location.href.indexOf('localhost') === -1) {
   // url = 'https://hasoc-annotation-backend.el.r.appspot.com/';
-  url = "https://shahbhavyan12-oxlf5aqfea-uc.a.run.app/";
+  url = "https://shahbhavyan12-oxlf5aqfea-uc.a.run.app";
   directoryName = 'https://annotationplatform.github.io/';
 } else {
    url = 'http://127.0.0.1:8080/';
