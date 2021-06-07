5be3cedaee7b58c2fae106ebcfdf000dd04fdc69
bhavyashahh <42242223+bhavyashahh@users.noreply.github.com>
2021-06-07 21:47:49 +0530

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -6,11 +6,14 @@ var LANGID=null;
 var TWEET_ID=null;
 if (window.location.href.indexOf('localhost') === -1) {
   // url = 'https://hasoc-annotation-backend.el.r.appspot.com/';
-  url = 'https://shahbhavyan12-oxlf5aqfea-uc.a.run.app/';
+ // url = 'https://shahbhavyan12-oxlf5aqfea-uc.a.run.app/';
+  
+  url = 'https://git-cd-oxlf5aqfea-uc.a.run.app/';
   directoryName = 'https://bhavyashahh.github.io//';
 } else {
   //url = 'http://127.0.0.1:8080/';
-  url = 'https://shahbhavyan12-oxlf5aqfea-uc.a.run.app/'
+  url = 'https://git-cd-oxlf5aqfea-uc.a.run.app/';
+  //url = 'https://shahbhavyan12-oxlf5aqfea-uc.a.run.app/'
   // url = 'https://hasoc-annotation-backend.el.r.appspot.com/';
   // url = "https://hasoc-backend-mgyikj6s3q-as.a.run.app/";
   // directoryName = '/annotationplatform.github.io/';
